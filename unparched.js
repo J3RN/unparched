65aa0f30a57cf90f975ae12e5cccf36613fd2f85
Jonathan Arnett <jonarnett90@gmail.com>
2015-11-21 13:47:36 -0500

    Working trend graph

--- unparched.js
+++ unparched.js
@@ -1,23 +1,44 @@
+Samples = new Meteor.Collection("samples");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Meteor.startup(function() {
+        var data = Samples.find({});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+        var graphData = data.map(function(dataPoint) {
+            return { x: dataPoint.time, y: dataPoint.level };
+        });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        console.log(graphData);
+
+        var graph = new Rickshaw.Graph({
+            element: document.querySelector("#chart"),
+            width: 580,
+            height: 250,
+            series: [ {
+                color: 'steelblue',
+                data: graphData
+            } ]
+        });
+
+        graph.render();
+    });
+
+    // Template.trend-chart.helpers({
+    //     data: function() {
+    //         return Samples.find({});
+    //     }
+    // });
+
+    // Template.hello.events({
+    //     'click button': function () {
+    //         // increment the counter when button is clicked
+    //         Session.set('counter', Session.get('counter') + 1);
+    //     }
+    // });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
